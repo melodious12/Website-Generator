5d17ffb67b32b835bec56c31339b0d7a3737d47f
melodious12 <zjulian10@gmail.com>
2025-05-22 19:35:14 -0400

    .

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,5 +1,5 @@
 import os
-import re
+from pathlib import Path
 from blocktype import markdown_to_html_node
 
 
@@ -25,12 +25,9 @@ def generate_page(from_path, template_path, dest_path, basepath):
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
 
-    template = template.replace('href="/', f'href="{basepath}')
-    template = template.replace('src="/', f'src="{basepath}')
-    template = template.replace('action="/', f'action="{basepath}')
-
-    template = re.sub(r'href="(?!\/|http)([^"]*)"', f'href="{basepath}\\1"', template)
-    template = re.sub(r'src="(?!\/|http)([^"]*)"', f'src="{basepath}\\1"', template)
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
+    template = template.replace('action="/', 'action="' + basepath)
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
@@ -47,15 +44,11 @@ def extract_title(md):
     raise ValueError("no title found")
 
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
-    entries = os.listdir(dir_path_content)
-    for entry in entries:
-        entry_path = os.path.join(dir_path_content, entry)
-        if os.path.isfile(entry_path) and entry.endswith('.md'):
-            rel_path = os.path.relpath(entry_path, dir_path_content)
-            dest_file_path = os.path.join(dest_dir_path, rel_path.replace('.md', '.html'))
-            generate_page(entry_path, template_path, dest_file_path, basepath)
-        elif os.path.isdir(entry_path):
-            rel_dir = os.path.relpath(entry_path, dir_path_content)
-            new_dest_dir = os.path.join(dest_dir_path, rel_dir)
-            os.makedirs(new_dest_dir, exist_ok=True)
-            generate_pages_recursive(entry_path, template_path, new_dest_dir, basepath)
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
